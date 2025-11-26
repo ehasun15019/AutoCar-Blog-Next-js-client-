066b37ef433a1f9d057cffdc79438e54f5363543
ehasun15019 <ehasunulislam0202@gmail.com>
2025-11-26 15:55:37 +0600

    update login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,15 @@
 import { assets } from "@/assets/assets";
 import LoginFrom from "@/Components/Login/LoginFrom";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function login() {
   return (
     <div className="py-8 px-4 sm:px-10 lg:px-20">
       <div className="flex flex-col md:flex-row items-center gap-10 justify-center">
-        <LoginFrom />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginFrom />
+        </Suspense>
         <Image
           src={assets.login}
           alt="register image"
